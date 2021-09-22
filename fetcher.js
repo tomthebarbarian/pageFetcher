62de2072448f9e995f00a5ce694e59c6ee85a3ce
Tom Zhang <tom.zhang989@gmail.com>
2021-09-22 19:58:37 -0400

    implemented the fetcher

--- fetcher.js
+++ fetcher.js
@@ -18,3 +18,70 @@
 // Inform is invalid
 // Inform if path is invalide
 // Inform if url is invalid
+const usableargs = process.argv.slice(2);
+const fs = require('fs');
+
+const content = 'Some content!';
+
+
+
+// if !length > 0
+
+// fs.writeFile(
+//   './index.html',
+//   content,
+//   err => {
+//     if (err) {
+//       console.error(err);
+//       return;
+//     }
+//   //file written successfully
+//   }
+// );
+
+
+// pos0 is https
+
+// pos1 is path
+
+
+const request = require('request');
+
+request(usableargs[0], (error, response, body) => {
+  if (error !== null) {
+    console.log('url not found');
+    return;
+  }
+  // console.log('error:', error); // Print the error if one occurred
+  // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  
+  // console.log('body:', body); // Print the HTML for the Google homepage.
+  fs.readFile(
+    usableargs[1],
+    'utf8' ,
+    (err, data) => {
+      if (data !== undefined) {
+        if (data.length > 0) {
+          console.log('the file already exists');
+          return;
+        }
+      }
+    }
+  );
+  fs.writeFile(usableargs[1], body, err => {
+    if (err) {
+      if (err.code === 'ENOTDIR') {
+        console.log('input path not found');
+        return;
+      }
+      console.error(err);
+      return;
+    }
+    //file written successfully
+    console.log('file write success');
+    let stats = fs.statSync(usableargs[1]);
+    console.log(`Downloaded and saved ${stats.size} bytes to ${usableargs[1]}`)
+
+  });
+});
+
