e4d3c9bbcb84d33713f8b28a58da22254b052b52
Tom Zhang <tom.zhang989@gmail.com>
2021-09-22 19:12:12 -0400

    created starting files

---
+++ fetcher.js
@@ -0,0 +1,20 @@
+// fecher.js should take two command line arguments:
+
+// a URL
+// a local file path
+// and downloads the resources at the remote url to the local path
+// upon completion, should print out a message about
+// downloaded quantity to location.
+
+
+// There are two operations in this problem which will take an unknown amount of time:
+
+// You need to make an http request and wait for the
+// response.
+// After the http request is complete, you need to take
+//  the data you receive and write it to a file in your local filesystem.
+
+// What if local file already exists?
+// Inform is invalid
+// Inform if path is invalide
+// Inform if url is invalid
